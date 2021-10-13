e8ebad44da57d8ba4bdf8e525785c748e2d97287
lumilio <logistic-division@lumilium.com>
2021-10-13 17:01:53 +0200

    aggiunta commenti

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,14 @@
 
 const theList = document.querySelector('#list');
 
-/* genero 100 indici con un loop for all'iterno di ul */
+// genero 100 li con un loop for all'iterno di ul 
 
 for (let i = 1; i <= 100; i++ ) {
     const theBox = document.createElement("li");
     theBox.append(i)
     theList.append(theBox)
 
-/* con le istruzioni condizionali cambio il colore dei box multipli di 3 e 5  */
+// con le istruzioni condizionali cambio il colore dei box multipli di 3 e 5 
 
     if (i % 3 == 0 && i % 5 == 0) {
         theBox.innerHTML = "fizzbuzz"

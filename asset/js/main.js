44505f93c215707de0e80dbedcaac9c67d6bb5a7
lumilio <logistic-division@lumilium.com>
2021-10-13 16:35:35 +0200

    aggiunta css

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,8 @@
 
-const theList = document.querySelector('#lista');
+const theList = document.querySelector('#list');
 
 for (let i = 1; i <= 100; i++ ){
-    const theBox = document.createElement("box");
+    const theBox = document.createElement("li");
     theBox.append(i)
     theList.append(theBox)
 } 
